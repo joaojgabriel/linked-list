9b0c186d492f0b8e6ba83b7529fb4518bf4d928c
Joao <jonhlhn.surf@gmail.com>
2023-05-16 08:26:55 -0300

    Fix append and prepend

--- src/LInkedList.js
+++ src/LInkedList.js
@@ -1,20 +1,43 @@
+/* eslint-disable no-plusplus */
 import Node from './Node';
 
 class LinkedList {
   constructor(value = null) {
-    this.head = value ? new Node(value) : undefined;
+    if (value) this.#addFirst(value);
+    else {
+      this.head = undefined;
+      this.tail = this.head;
+      this.size = 0;
+    }
+  }
+
+  #addFirst(value) {
+    const node = new Node(value);
+    this.head = node;
     this.tail = this.head;
-    this.size = value ? 1 : 0;
+    this.size = 1;
+    return node;
   }
 
   append(value) {
-    this.tail.next = new Node(value);
-    this.tail = this.tail.next;
+    this.size++;
+    if (this.size === 1) {
+      return this.#addFirst(value);
+    }
+    const node = new Node(value);
+    this.tail.next = node;
+    this.tail = node;
+    return node;
   }
 
   prepend(value) {
+    this.size++;
+    if (this.size === 1) {
+      return this.#addFirst(value);
+    }
     const node = new Node(value);
     node.next = this.head;
     this.head = node;
+    return node;
   }
 }
